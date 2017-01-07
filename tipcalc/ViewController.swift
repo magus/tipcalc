03a5e4f29b76fb6399a14d08304e13b12e660c8e
noah <noah@iamnoah.com>
2017-01-07 00:12:42 -0800

    Initial Commit

---
+++ tipcalc/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tipcalc
+//
+//  Created by Noah Jorgensen on 1/7/17.
+//  Copyright © 2017 Noah Jorgensen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
